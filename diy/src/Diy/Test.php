29d9478cc97e99d8bf193f07826d27780dd7e448
hyx <huangyx@swg.com.cn>
2017-01-19 15:43:22 +0800

    hi

---
+++ diy/src/Diy/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hyx
+ * Date: 17-1-19
+ * Time: 下午3:36
+ */
+
+namespace Diy;
+
+
+class Test
+{
+    public function display()
+    {
+        echo 'hi composer!';
+    }
+}
